93a1b5f044e3cc2db65168977dd43309275db2fd
mhkmcp <mhkmcp@yahoo.com>
2020-02-19 17:16:28 +0600

    Added Review with Testing

--- books/models.py
+++ books/models.py
@@ -1,7 +1,7 @@
 import uuid
-
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class Book(models.Model):
@@ -17,4 +17,20 @@ class Book(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('book_detail', args=[self.id])
+        return reverse('book_detail', kwargs={'pk': str(self.pk)})
+
+
+class Review(models.Model): # new
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+    )
+    review = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.review
--- books/urls.py
+++ books/urls.py
@@ -4,5 +4,5 @@ from .views import BookListView, BookDetailView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
-    path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'),
+    path('<int:pk>', BookDetailView.as_view(), name='book_detail'),
 ]
