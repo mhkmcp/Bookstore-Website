bd2cd82426165b3db7fe3f63d9645a86407958cc
mhkmcp <mhkmcp@yahoo.com>
2020-02-24 12:37:45 +0600

    Added Search Feature

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
@@ -19,4 +20,17 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'    # models.py
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    # queryset = Book.objects.filter(title__icontains='beginners')  # new
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
 
